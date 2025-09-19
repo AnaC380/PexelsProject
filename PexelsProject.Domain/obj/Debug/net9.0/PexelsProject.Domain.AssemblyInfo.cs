367f5f2008347a84dfcf57bfba12bd5594fe50fa
AnaC380 <anasalles380@gmail.com>
2025-09-19 10:17:23 -0300

    Atualiza as configurações nos arquivos appsettings

--- PexelsProject.Domain/obj/Debug/net9.0/PexelsProject.Domain.AssemblyInfo.cs
+++ PexelsProject.Domain/obj/Debug/net9.0/PexelsProject.Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PexelsProject.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+394b6f08288ad1f70c024e4fdfe3088641ba4da1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+63d7bbdf7edd67e67567f76ca3cebfbb3511d123")]
 [assembly: System.Reflection.AssemblyProductAttribute("PexelsProject.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PexelsProject.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
